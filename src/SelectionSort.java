8bd9829a05c5546c36aa199425c7b141726e4cbb
Danish Thomas <a.danishthomas@gmail.com>
2022-10-04 13:48:53 +0530

    Selection Sort added

---
+++ src/SelectionSort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {1,-2,3,11,5,32};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void selectionSort(int[] arr){
+
+        for(int i=0; i< arr.length; i++){
+            int last = arr.length -i -1;
+            int maxIndex = getMaxIndex(arr, last);
+            swap(arr, maxIndex, last);
+        }
+
+    }
+
+     static int getMaxIndex(int[] arr, int end) {
+
+        int max = 0;
+        for(int i = 0; i<=end; i++){
+            if(arr[max]< arr[i]){
+                max=i;
+            }
+        }
+        return max;
+
+    }
+
+    static void swap(int[] arr, int first, int second){
+
+        int temp = arr[first];
+        arr[first]= arr[second];
+        arr[second] = temp;
+
+    }
+}
